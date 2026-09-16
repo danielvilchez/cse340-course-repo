abd030e2bcfab407837ca1a5e59f853bd58c719b
Daniel Vilchez Cardenas <daniel2005vc@gmail.com>
2026-09-16 11:47:49 -0500

    Add organization details route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
-import { showOrganizationsPage } from './controllers/organizations.js';
+import { showOrganizationsPage, showOrganizationDetailsPage } from './controllers/organizations.js';
 import { showProjectsPage } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
@@ -15,5 +15,8 @@ router.get('/categories', showCategoriesPage);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
+
+// Route for organization details page
+router.get('/organization/:id', showOrganizationDetailsPage);
 
 export default router;
